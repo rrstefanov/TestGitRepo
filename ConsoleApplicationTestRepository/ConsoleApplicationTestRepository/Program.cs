2ee19ac21fd50708547b481f337495c5f55ef6e3
rrstefanov <rrstefanov@hotmail.com>
2018-03-10 22:34:56 +0200

    Create test project

---
+++ ConsoleApplicationTestRepository/ConsoleApplicationTestRepository/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplicationTestRepository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
